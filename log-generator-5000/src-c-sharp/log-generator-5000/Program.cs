9328760dad573828ab228d0644b7fe33448775f1
Brandon Nicoll <bgnicoll@gmail.com>
2023-01-17 09:02:22 -0600

    Docker and env variables | Log Generator 5000 C# init

---
+++ log-generator-5000/src-c-sharp/log-generator-5000/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace log_generator_5000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
