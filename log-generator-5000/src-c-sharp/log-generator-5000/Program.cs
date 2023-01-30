2b2ed7addef4d70c90ba658763a2055d5ed7cd52
Brandon Nicoll <bgnicoll@gmail.com>
2023-01-30 17:57:04 -0600

    Serilog and Filebeat config

--- log-generator-5000/src-c-sharp/log-generator-5000/Program.cs
+++ log-generator-5000/src-c-sharp/log-generator-5000/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace log_generator_5000
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
+
+namespace log_generator_5000
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Log Generator 5000 is ready to generate logs");
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.File(new ExpressionTemplate(
+                     "{ {timestamp: UtcDateTime(@t), level: @l, message: @m, exception: @x, ..@p} }\n"),
+                     "logs/log-generator-5000.log",
+                     rollOnFileSizeLimit: true,
+                     fileSizeLimitBytes: (50 * 1024 * 1024),
+                     retainedFileCountLimit: 5)
+                .WriteTo.Console(new ExpressionTemplate(
+                     "[{UtcDateTime(@t)} {@l:u3}] {@m}\n{@x}", theme: TemplateTheme.Code))
+                .CreateLogger();
+
+            var reallyImportantProperty = "This property must be logged if I am to troubleshoot later";
+
+            logger.Information("Log Generator 5000 is ready to generate logs | reallyImportantProperty: {prop} | Sound the alarm? {alarm}", reallyImportantProperty, true);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                logger.Information("Iteration {i}", i);
+            }
+
+            var customerId = 1234;
+            var customerEmailAddress = "brandon@example.com";
+            try
+            {
+                throw new Exception("Well that didn't work");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception thrown in Program.cs. customerId: {customerId} | customerEmailAddress: {customerEmailAddress}", customerId, customerEmailAddress);
+            }
         }
     }
 }
